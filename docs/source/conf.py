32ddb3b1a5961a92f82a7561e65a1e71e21bff52
danielee0707 <danielee0707@gmail.com>
2022-05-08 19:54:18 -0400

    rtd doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,15 +4,13 @@ import logging
 from datetime import datetime
 from pathlib import Path
 from sphinx.ext import autosummary
-
+import multivelo
 import matplotlib
 matplotlib.use('agg')
 
 HERE = Path(__file__).parent
 sys.path.insert(0, str(HERE.parent.parent))
 sys.path.insert(0, os.path.abspath("_ext"))
-
-import multivelo
 
 # -- Project information
 
@@ -32,6 +30,8 @@ extensions = [
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
 ]
+
+autosummary_generate = True
 
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
